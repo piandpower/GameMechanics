3dd7b4f50a6f2a9efc78e746788c3dea1053f073
rkorovin <latedeath@gmail.com>
2017-06-08 01:20:59 +0300

    Remove default comments in ScoreBoard.

--- Source/GameMechanics/GDK/Components/ScoreBoard.cpp
+++ Source/GameMechanics/GDK/Components/ScoreBoard.cpp
@@ -3,14 +3,9 @@
 #include "ScoreBoard.h"
 
 
-// Sets default values for this component's properties
 UScoreBoard::UScoreBoard()
 {
-	// Set this component to be initialized when the game starts, and to be ticked every frame.  You can turn these features
-	// off to improve performance if you don't need them.
 	PrimaryComponentTick.bCanEverTick = true;
-
-	// ...
 }
 
 
@@ -18,9 +13,6 @@ UScoreBoard::UScoreBoard()
 void UScoreBoard::BeginPlay()
 {
 	Super::BeginPlay();
-
-	// ...
-	
 }
 
 
